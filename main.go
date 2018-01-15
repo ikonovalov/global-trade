2fea99650c6cd506dcc5b84bf4e6e629cd4d8ad0
ikonovalov <konovalov84@gmail.com>
2018-01-16 00:44:23 +0300

    Refactoring and learn golang deeper

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"errors"
+	"os"
 )
 
 const (
@@ -14,38 +16,62 @@ const (
 	ApiVersion = "3"
 )
 
-func main() {
+func tickers(pairs string) string {
+	return ApiBase + ApiVersion + "/ticker/" + pairs
+}
 
-	url := ApiBase + ApiVersion + "/ticker/xem_usd-eth_usd-xem_eth"
-	fmt.Println(Bold(url+"\n"))
+func unmarshal(data [] byte, obj interface{}) error {
+	return json.Unmarshal(data, obj)
+}
+
+func query(client *http.Client, url string) ([]byte) {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		log.Fatal("NewRequest: ", err)
-		return
+		panic(err)
 	}
-
-	client := &http.Client{}
 
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Fatal("Do: ", err)
-		return
+		panic(err)
 	}
-
 	defer resp.Body.Close()
-	response, _ := ioutil.ReadAll(resp.Body)
-
 	if resp.StatusCode != 200 {
 		fmt.Errorf("error. HTTP %d", resp.StatusCode)
-	} else {
-		var tickerResponse TickerInfoResponse
-		if err := json.Unmarshal(response, &tickerResponse.Tickers); err != nil {
-			panic(err)
-		}
-		for ticker, v := range tickerResponse.Tickers {
-			fmt.Printf("%s High [%f]\t Low[%f]\t Volume[%f]\t Current Volume[%f]\n",
-				Bold(ticker), v.High, v.Low, v.Vol, v.VolCur)
-		}
-
+		panic(errors.New("Status is not 200. It's " + string(resp.StatusCode)))
 	}
+	response, _ := ioutil.ReadAll(resp.Body)
+	return response
+}
+
+func main() {
+
+	argsWithProg := os.Args
+	var pair string
+	if len(argsWithProg) == 2 {
+		pair = argsWithProg[1]
+	} else {
+		pair = "xem_usd-eth_usd-xem_eth"
+	}
+
+	url := tickers(pair)
+	fmt.Println(Bold(url + "\n"))
+
+	client := &http.Client{}
+	response := query(client, url)
+
+	var tickerResponse TickerInfoResponse
+	pTicker := &tickerResponse.Tickers
+
+	if err := unmarshal(response, pTicker); err != nil {
+		panic(err)
+	}
+
+	// print result
+	for ticker, v := range tickerResponse.Tickers {
+		fmt.Printf("%s High [%f]\t Avg [%f]\t Low[%f]\t Volume[%f]\t Current Volume[%f]\n",
+			Bold(ticker), v.High, Green(v.Avg), v.Low, v.Vol, v.VolCur)
+	}
+
 }
